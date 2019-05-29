2b16e8a08384a3923d76a61a246d7ef3b2f7406c
mohitsharma23 <mohitsharma23j97@gmail.com>
2019-05-29 12:38:03 +0530

    Updates

---
+++ precache-manifest.a3bc3946106aa62f9b270036761e9fa0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/github_card/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e3a9e72fb788ace6840c",
+    "url": "/github_card/static/css/main.2ee86f50.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/github_card/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "da45924662a060da4d16",
+    "url": "/github_card/static/js/1.da459246.chunk.js"
+  },
+  {
+    "revision": "c0ee22562f5722878c05",
+    "url": "/github_card/static/js/runtime~main.c0ee2256.js"
+  },
+  {
+    "revision": "e3a9e72fb788ace6840c",
+    "url": "/github_card/static/js/main.e3a9e72f.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/github_card/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/github_card/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/github_card/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "da45924662a060da4d16",
+    "url": "/github_card/static/css/1.0added89.chunk.css"
+  },
+  {
+    "revision": "faa03aba00a3660347806dd2893998d7",
+    "url": "/github_card/index.html"
+  }
+];
